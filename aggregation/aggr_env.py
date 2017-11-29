0d147ee3aefcb3bf96841a53c3b31be4868f8962
yangliu28 <yxl1450@case.edu>
2017-11-28 22:51:03 -0500

    add ico

--- aggregation/aggr_env.py
+++ aggregation/aggr_env.py
@@ -11,16 +11,23 @@ from Tkinter import *
 class AggrEnv():  # abbreviation for aggregation environment
     def __init__(self, robot_quantity):
         self.N = robot_quantity
-        self.root = Tk()  # root window, as the simulation window
+        # root window, as the simulation window
+        self.root = Tk()
         self.root.resizable(width=False, height=False)  # inhibit resizing
         self.root.geometry('600x600+100+100')  # widthxheight+xpos+ypos
         self.root.title('Swarm Aggregation with RL of Policy Gradient')
+        # self.root.iconbitmap('../ants.png')
+        ico_img = PhotoImage(file='../ants.png')
+        self.root.tk.call('wm', 'iconphoto', self.root._w, ico_img)
+        # canvas for all drawing
         self.canvas = Canvas(self.root)
         self.canvas.pack(fill=BOTH)  # fill entire window
         self.dot1 = self.canvas.create_oval(10,10,30,30)
-        # self.root.mainloop()
-        print('program goes here')
+        # self.root.mainloop()  # not needed, have my own loop
 
     def frame_update(self, x, y):
         self.canvas.move(self.dot1, x, y)
         self.root.update()
+
+
+
