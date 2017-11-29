adb89e3dd61085ebe870f7076cd78aa09006f2ed
yangliu28 <yxl1450@case.edu>
2017-11-28 21:28:14 -0500

    embracing tkinter

--- aggregation/aggr_env.py
+++ aggregation/aggr_env.py
@@ -1,4 +1,4 @@
-# 
+# class for aggregation simulation environment
 
 # hold all data and computation related to the physical environment
 
@@ -6,4 +6,21 @@
 # and take actions as input, calculate the reward, and output new observation again
 
 
+from Tkinter import *
 
+class AggrEnv():  # abbreviation for aggregation environment
+    def __init__(self, robot_quantity):
+        self.N = robot_quantity
+        self.root = Tk()  # root window, as the simulation window
+        self.root.resizable(width=False, height=False)  # inhibit resizing
+        self.root.geometry('600x600+100+100')  # widthxheight+xpos+ypos
+        self.root.title('Swarm Aggregation with RL of Policy Gradient')
+        self.canvas = Canvas(self.root)
+        self.canvas.pack(fill=BOTH)  # fill entire window
+        self.dot1 = self.canvas.create_oval(10,10,30,30)
+        # self.root.mainloop()
+        print('program goes here')
+
+    def frame_update(self, x, y):
+        self.canvas.move(self.dot1, x, y)
+        self.root.update()
--- aggregation/aggr_main.py
+++ aggregation/aggr_main.py
@@ -3,4 +3,24 @@
 # interact with the environment, and brain of reinforcement learning
 
 
+from aggr_env import AggrEnv
+import time
+
+sim_env = AggrEnv(30)
+
+sleep_time = 0.1
+
+while True:
+    for _ in range(20):
+        sim_env.frame_update(5,0)
+        time.sleep(sleep_time)
+    for _ in range(20):
+        sim_env.frame_update(0,5)
+        time.sleep(sleep_time)
+    for _ in range(20):
+        sim_env.frame_update(-5,0)
+        time.sleep(sleep_time)
+    for _ in range(20):
+        sim_env.frame_update(0,-5)
+        time.sleep(sleep_time)
 
